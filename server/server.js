881bbf1f8e89635b854c5e50a302ba2f4f4af599
Ranjith <ranjithram878@gmail.com>
2025-10-08 10:40:21 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -9,15 +9,15 @@ import packageRoutes from "./routes/packageRoutes.js";
 dotenv.config();
 connectDB();
 
-const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
         origin: process.env.CLIENT_URL,
         credentials: true,
     }
 ));
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 
 
 // Routes
